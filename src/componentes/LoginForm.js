f7086d31ee5bb4bd02240f7dc46d8f98a08a1a2a
YamilaGaleano <yamila@retailsystem.com>
2023-07-29 20:44:28 -0300

    componetizando login

---
+++ src/componentes/LoginForm.js
@@ -0,0 +1,20 @@
+
+const LoginForm = () => {
+  return (
+    <form className="row g-3 align-items-center mb-5">
+    <div className="col-12">
+      <label htmlFor="usuLogin" className="form-label">Usuario</label>
+      <input type="text" className="form-control" id="usuLogin" placeholder=""  />
+    </div>
+    <div className="col-12">
+      <label htmlFor="passLogin" className="form-label">Password</label>
+      <input type="password" className="form-control" id="passLogin" placeholder=""  />
+    </div>
+    <div className="col-12">
+      <button type="submit" className="btn btn-primary" >Iniciar sesión</button>
+    </div>
+  </form>
+  )
+}
+
+export default LoginForm
---
+++ src/componentes/LogoPrincipal.js
@@ -0,0 +1,11 @@
+const LogoPrincipal = () => {
+  return (
+    <div className="row mb-5">
+    <div className="col text-center">
+      <h1>Censoapp</h1>
+    </div>
+  </div>
+  )
+}
+
+export default LogoPrincipal
