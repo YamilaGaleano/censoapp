368335b58a4f69de94ee873ea73ca2fe0297a5aa
FacuCalda <facundocalda79@gmail.com>
2023-07-29 20:27:37 -0300

    login

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -1,80 +1,59 @@
 import React from 'react'
 
 import { useRef, useState } from "react"
+import FormLogin from './FormLogin'
+import LoginContenedor from './LoginContenedor'
+import LogoPrincipal from './LogoPrincipal'
 
 const Login = () => {
-  const user = useRef(null);
-  const pass = useRef(null);
-  const [errorUser, setErrorUser] = useState(false)
+  // const user = useRef(null);
+  // const pass = useRef(null);
+  // const [errorUser, setErrorUser] = useState(false)
 
-  const validar = () => {
-    const usuario = {
-      userCampo: user.current.value,
-      passCampo: pass.current.value
-    }
+  // const validar = () => {
+  //   const usuario = {
+  //     userCampo: user.current.value,
+  //     passCampo: pass.current.value
+  //   }
 
-    //mejorar esta validacion
-    if (usuario.userCampo !== "" && usuario.passCampo !== "") {
-      return usuario
-    }
-    else {
-      setErrorUser(true);
-      return null;
-    }
-  }
+  //   //mejorar esta validacion
+  //   if (usuario.userCampo !== "" && usuario.passCampo !== "") {
+  //     return usuario
+  //   }
+  //   else {
+  //     setErrorUser(true);
+  //     return null;
+  //   }
+  // }
 
-  const ingresar = (event) => {
-    event.preventDefault();
-    const usu = validar();
-    if (usu != null) {
-      fetch(`https://censo.develotion.com/login.php`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(usu)
-      }).then(r => r.json())
-        .then(data => {
-          if (data.codigo == 200) {
-            localStorage.setItem("apikey", data.apiKey);
-            localStorage.setItem("id", data.id);
-            //ACA CREO QUE REDIRECCIONAR A ALGUN LADO
-            console.log("Login exitoso");
-          } else {
-            console.log(data.mensaje);
-          }
-        })
-    }
+  // const ingresar = (event) => {
+  //   event.preventDefault();
+  //   const usu = validar();
+  //   if (usu != null) {
+  //     fetch(`https://censo.develotion.com/login.php`, {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       },
+  //       body: JSON.stringify(usu)
+  //     }).then(r => r.json())
+  //       .then(data => {
+  //         if (data.codigo == 200) {
+  //           localStorage.setItem("apikey", data.apiKey);
+  //           localStorage.setItem("id", data.id);
+  //           //ACA CREO QUE REDIRECCIONAR A ALGUN LADO
+  //           console.log("Login exitoso");
+  //         } else {
+  //           console.log(data.mensaje);
+  //         }
+  //       })
+  //   }
 
-  }
+  // }
   return (
     <div className="container-fluid d-flex flex-column justify-content-center align-items-center fondo" style={{ minHeight: "100vh" }}>
-      <div className="row mb-5">
-        <div className="col text-center">
-          <h1>Censoapp</h1>
-        </div>
-      </div>
-      <div className="border p-4 rounded-3 text-center login-registro">
-        <h1 className="mb-5">Login</h1>
-        <form className="row g-3 align-items-center mb-5">
-          <div className="col-12">
-            <label htmlFor="usuLogin" className="form-label">Usuario</label>
-            <input type="text" className="form-control" id="usuLogin" placeholder="" ref={user} />
-          </div>
-          <div className="col-12">
-            <label htmlFor="passLogin" className="form-label">Password</label>
-            <input type="password" className="form-control" id="passLogin" placeholder="" ref={pass} />
-          </div>
-          <div className="col-12">
-            <button type="submit" className="btn btn-primary" onClick={ingresar}>Iniciar sesión</button>
-          </div>
-        </form>
-        {errorUser &&
-          <p className="mb-5 mt-5">Usuario y/o contraseña incorrectos</p>
-        }
-        <a href="/registro">Registrarse</a>
-
-      </div>
+        <LogoPrincipal/>
+      <LoginContenedor/>
     </div>
 
   )
---
+++ src/componentes/LoginContenedor.js
@@ -0,0 +1,17 @@
+import FormLogin from "./FormLogin"
+
+const LoginContenedor = () => {
+  return (
+    <div className="border p-4 rounded-3 text-center login-registro">
+    <h1 className="mb-5">Login</h1>
+  <FormLogin/>
+    {/* {errorUser &&
+      <p className="mb-5 mt-5">Usuario y/o contraseña incorrectos</p>
+    } */}
+    <a href="/registro">Registrarse</a>
+
+  </div>
+  )
+}
+
+export default LoginContenedor
