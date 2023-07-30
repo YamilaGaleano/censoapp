a87673b6f725496858a47805ece506d0923e1773
YamilaGaleano <yamila@retailsystem.com>
2023-07-29 22:31:42 -0300

    changes

--- src/componentes/LoginContenedor.js
+++ src/componentes/LoginContenedor.js
@@ -1,2 +1,16 @@
+import LoginForm from "./LoginForm"
 
+const LoginContenedor = () => {
+  return (
+    <div className="border p-4 rounded-3 text-center login-registro">
+    <h1 className="mb-5">Login</h1>
+    <LoginForm />
+    {/* {errorUser &&
+      <p className="mb-5 mt-5">Usuario y/o contraseña incorrectos</p>
+    } */}
+    <a href="/registro">Registrarse</a>
+
+  </div>
+  )
+}
 export default LoginContenedor
