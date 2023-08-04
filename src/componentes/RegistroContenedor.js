02eba08d1235fc58aa53af3b63d16cf3db3a4954
YamilaGaleano <yamila@retailsystem.com>
2023-08-04 20:38:44 -0300

    registro

--- src/componentes/RegistroContenedor.js
+++ src/componentes/RegistroContenedor.js
@@ -1,15 +1,49 @@
+import { useState } from "react"
 import RegistroForm from "./RegistroForm"
+import { useNavigate } from "react-router-dom";
 
 const RegistroContenedor = () => {
+  const navigate = useNavigate(null);
+  const [errorRegistro, setErrorRegistro] = useState(false);
+
+  const altaRegistro = (datos) => {
+
+    console.log(datos);
+
+    fetch(`https://censo.develotion.com/usuarios.php`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(datos)
+    }).then(r => r.json())
+      .then(data => {
+        console.log("data", data)
+        if (data.codigo === 200) {
+          localStorage.setItem("apikey", data.apiKey);
+          localStorage.setItem("id", data.id);
+          navigate("/Dashboard")
+          console.log("Login exitoso");
+        } else {
+          console.log(data.mensaje);
+          setErrorRegistro(true);
+        }
+      })
+
+
+  }
+
   return (
     <div className="border p-4 rounded-3 text-center login-registro">
-    <h1 className="mb-5">Registraro</h1>
-    <RegistroForm/>
-    <p className="mb-5 mt-5">Usuario y/o contraseña incorrectos</p>
-    
-    <a href="/">Registrarse</a>
+      <h1 className="mb-5">Registro</h1>
+      <RegistroForm altaRegistro={altaRegistro} />
+      {errorRegistro &&
+        <p className="mb-5 mt-5">Usuario y/o contraseña incorrectos</p>
+      }
 
-  </div>
+      <a href="/">Volver al login</a>
+
+    </div>
   )
 }
 
--- src/componentes/RegistroForm.js
+++ src/componentes/RegistroForm.js
@@ -1,17 +1,27 @@
+import { useRef } from "react"
 
-const RegistroForm = () => {
+const RegistroForm = ({altaRegistro}) => {
+    const usu = useRef(null);
+    const pass = useRef(null);
+    
+    const tomarDatos =(e)=>{
+        e.preventDefault();
+        const datos = {usuario: usu.current.value, password: pass.current.value};
+        altaRegistro(datos);
+    }
+
     return (
         <form className="row g-3 align-items-center mb-5">
             <div className="col-12">
-                <label htmlFor="usuRegistro" className="form-label">Usuario</label>
-                <input type="text" className="form-control" id="usuRegistro" placeholder="" />
+                <label htmlFor="usuRegistro" className="form-label" >Usuario</label>
+                <input type="text" className="form-control" id="usuRegistro" placeholder=""  ref={usu}/>
             </div>
             <div className="col-12">
-                <label htmlFor="passRegistro" className="form-label">Password</label>
-                <input type="password" className="form-control" id="passRegistro" placeholder="" />
+                <label htmlFor="passRegistro" className="form-label" >Password</label>
+                <input type="password" className="form-control" id="passRegistro" placeholder=""  ref={pass}/>
             </div>
             <div className="col-12">
-                <button type="submit" className="btn btn-primary" >Registrarse</button>
+                <input type="submit" className="btn btn-primary" onClick={tomarDatos} value="Registrarse"/>
             </div>
         </form>
     )
